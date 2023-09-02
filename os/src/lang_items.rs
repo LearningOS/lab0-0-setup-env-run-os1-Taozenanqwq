684fde6594a24ac25026d8283abbd04599a09a45
tzn <tzn1104@126.com>
2023-09-02 06:49:47 +0000

    完成ch2

--- os/src/lang_items.rs
+++ os/src/lang_items.rs
@@ -1,6 +1,5 @@
 use crate::sbi::shutdown;
 use core::panic::PanicInfo;
-use crate::stack_trace::print_stack_trace;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -14,6 +13,5 @@ fn panic(info: &PanicInfo) -> ! {
     } else {
         println!("Panicked: {}", info.message().unwrap());
     }
-    unsafe{print_stack_trace();}
     shutdown()
 }
